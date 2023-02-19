bf22e1c63f3ffc658e0ec83485751436b3a09921
emirkanozer <emirkan.ozer@outlook.com>
2023-02-19 19:11:29 +0300

    chore

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import Input from "../Input";
+import styles from "./styles.module.css";
+function Card() {
+  return <div className={styles.wrapper}>{/* <Input></Input> */}</div>;
+}
+
+export default Card;
